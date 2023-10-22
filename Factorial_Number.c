a4297e63a59b4792456a93d7fc47a946cb9b6e46
Sakhawat Hossain <141615599+Sakhawat025@users.noreply.github.com>
2023-10-22 22:27:31 +0600

    Add files via upload

---
+++ Factorial_Number.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/*int main() {
+    int n;
+    unsigned long long factorial = 1;
+
+    printf("Enter a non-negative integer: ");
+    scanf("%d", &n);
+
+    if (n < 0) {
+        printf("Factorial is not defined for negative numbers.\n");
+    } else {
+        for (int i = 1; i <= n; i++) {
+            factorial *= i;
+        }
+
+        printf("Factorial is %llu\n",factorial);
+    }
+
+    return 0;
+}*/
+
+int factorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num < 0) {
+        printf("Factorial is not defined for negative numbers.\n");
+    } else {
+        int result = factorial(num);
+        printf("Factorial is %d\n",result);
+    }
+
+    return 0;
+
+}
+
