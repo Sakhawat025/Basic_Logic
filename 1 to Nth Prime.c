4f3a68b5281b55d79386897ecd48b94a6be7c478
Sakhawat Hossain <141615599+Sakhawat025@users.noreply.github.com>
2023-11-21 22:11:11 +0600

    Add files via upload

---
+++ 1 to Nth Prime.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+// Function to check if a number is prime
+bool isPrime(int num) {
+    if (num <= 1) {
+        return false;
+    }
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Function to print prime numbers from 1 to n
+void printPrimes(int n) {
+    printf("Prime numbers from 1 to %d are: \n", n);
+    for (int i = 2; i <= n; i++) {
+        if (isPrime(i)) {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+
+    // Get user input for the upper limit
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    // Print prime numbers from 1 to n
+    printPrimes(n);
+
+    return 0;
+}
