a7aa54cf8fc9a649a0e254fdf689a2db5f6f8ef7
Sakhawat Hossain <141615599+Sakhawat025@users.noreply.github.com>
2023-12-23 18:29:51 +0600

    Add files via upload

---
+++ Binary_Operators.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// CPP Program to demonstrate the Binary Operators.
+/*int main()
+{
+    int a,b;
+    cin >> a >> b;
+    cout << "a+b= " <<(a+b) << endl;
+    cout << "a-b= " <<(a-b) << endl;
+    cout << "a*b= " <<(a*b) << endl;
+    cout << "a/b= " <<(a/b) <<endl;
+
+    return 0;
+}*/
+
+// // CPP Program to demonstrate the Relational Operators
+/*int main()
+{
+    int a,b;
+    cin >> a >> b;
+    cout << "a == b is " << (a == b) << endl;
+    cout << "a > b is " << (a > b) << endl;
+    cout << "a >= b is " << (a >= b) << endl;
+    cout << "a < b is " << (a < b) << endl;
+    cout << "a <= b is " << (a <= b) << endl;
+    cout << "a != b is " << (a != b) << endl;
+
+    return 0;
+
+}*/
+// CPP Program to demonstrate the Bitwise Operators
+/*int main()
+{
+    int a,b;
+    cin >> a>> b;
+    cout << "a & b is " << (a & b) << endl;
+    cout << "a | b is " << (a | b) << endl;
+    cout << "a ^ b is " << (a ^ b) << endl;
+    cout << "a<<1 is " << (a << b) << endl;
+    cout << "a>>1 is " << (a >> b) << endl;
+    cout << "~(a) is " << ~(a) << endl;
+
+    return 0;
+}*/
+// CPP Program to demonstrate the Assignment Operators
+/*int main()
+{
+    int a,b;
+    cin >> a>> b;
+    cout << "a = " << a << endl;
+    cout << "a += b is " << (a += b) << endl;
+    cout << "a -= b is " << (a -= b) << endl;
+    cout << "a *= b is " << (a *= b) << endl;
+    cout << "a /= b is " << (a /= b) << endl;
+
+    return 0;
+}*/
+
+// CPP Program to demonstrate the Conditional Operators
+int main()
+{
+    int a,b;
+    cin >> a>> b;
+    int result = (a < b) ? b : a;
+    cout << "The greatest number is " << result << endl;
+
+    return 0;
+}
