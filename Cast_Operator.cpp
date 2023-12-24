100e3d45a51753800ba2357b4b1df97397571ff6
Sakhawat Hossain <141615599+Sakhawat025@users.noreply.github.com>
2023-12-24 18:16:20 +0600

    Add files via upload

---
+++ Cast_Operator.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double a= 77.7879;
+    float b=76.77;
+    int c;
+
+    c=(int)a;
+    cout << "Line 1 - value of (int)a is :" << c << endl;
+
+    c=(int)b;
+    cout << "Line 2 - Value of (int)b is :" << c << endl;
+
+    return 0;
+}
