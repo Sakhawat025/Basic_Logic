a3c45fc8e086e5102559da05ed4c7a9602edcf2f
Sakhawat Hossain <141615599+Sakhawat025@users.noreply.github.com>
2024-01-04 20:18:24 +0600

    Add files via upload

---
+++ Comma_Operator.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// comma as a operator.
+/*int main()
+{
+    int num1=(10,24,30);
+    cout << num1 << endl;
+
+    return 0;
+}*/
+
+// coma as a separator operator.
+/*int main()
+{
+    int n1=76,n2=77,n3=78;
+    cout << n1 << " " << n2 << " " << n3 <<endl;
+
+    return 0;
+}*/
+// Multiple arguments is in a function call, where we used commas as a separator.
+
+/*int Addition(int n1,int n2,int n3)
+{
+    int r=n1+n2+n3;
+    return r;
+}
+int main()
+{
+    int n1=76,n2=77,n3=78;
+    int add=Addition(n1,n2,n3);
+
+    cout << add << endl;
+
+    return 0;
+}*/
+
+// Comma Operator in Place of a Semicolon
+int main()
+{
+    int n1=76,n2=77,n3=78;
+    cout << n1 << endl, cout << n2 << endl, cout << n3 << endl;
+
+    return 0;
+
+}
+
